b1061e2a113d4246aa37b8cef0a680fb64078c50
Dmitry Bezborodov <61113740+njsx@users.noreply.github.com>
2023-06-11 13:48:27 +0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CodeIgniter 4 Notes'
+copyright = '2023, njsx'
+author = 'Дмитрий Безбородов'
 
 release = '0.1'
 version = '0.1.0'
